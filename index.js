d67b746495dc1963e12fa459e109705ca4d4e0cb
dcozartII <85534666+dcozartII@users.noreply.github.com>
2021-09-06 22:36:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', function(req, res) {
 
     res.send('<h3>Hello Node on Heroku</h3>');
-    res.send('<a href="https://github.com/dcozartII/hello-heroku-app">Github Repo</a>');
+    
 
 });
 
